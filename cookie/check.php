1a77b3008759f1dc55998e9a61f9974559c88676
mackliu <macklun@gmail.com>
2025-05-19 09:54:45 +0800

    使用cookie來完成會員登入的動作

--- cookie/check.php
+++ cookie/check.php
@@ -2,6 +2,7 @@
 
 if($_POST['acc'] == 'admin' && $_POST['pw'] == '1234'){
     echo "登入成功";    
+    setcookie("login", 1, time()+180);
     header("location:login.php?login=1");
 }else{
     echo "登入失敗";
